3873a5ab74e2c8238ab8f2c437ac5925f71b54dd
lukasg33 <lukasg33@uw.edu>
2016-10-06 15:16:16 -0700

    participation

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,6 +2,15 @@
 
 # Write a function `AddThree` that adds 3 to an input value
 
+addthree <- function(a)
+ { answer <- a+3
+  return(answer)}
+
+ten <- addthree(7)
+
+feetToMeters <- function(a){
+  answer <- a/3.28
+  return(answer)}
 
 # Create a variable `ten` by passing 7 to your `AddThree` function
 
@@ -12,4 +21,4 @@
 # Create a variable `height.in.feet` that is your height in feet
 
 
-# Create a variable `height.in.meters` by passing `height.in.feet` to your `FeetToMeters` function
+fee# Create a variable `height.in.meters` by passing `height.in.feet` to your `FeetToMeters` function
